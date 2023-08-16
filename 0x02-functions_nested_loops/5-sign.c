ddac25f06d0ace69c8b60178e9ef9a5dfdd4ba1a
Chimenem <chimenemwillis@gmail.com>
2023-08-15 18:27:33 -0700

    Write a function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,9 @@
  */
 int print_sign(int n)
 {
+	int negative = -1;
+	char ch = (char) negative;
+
 	if (n > 0)
 	{
 		_putchar('+');
@@ -20,7 +23,7 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (48);
+		return (ch);
 	}
 
 }
